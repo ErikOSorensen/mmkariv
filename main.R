94699b22450c95c9b5413273b2532824d6e5a2b6
Erik Ø. Sørensen <erik.sorensen@nhh.no>
2023-06-21 18:37:05 +0200

    Script to run all calculations at once.

---
+++ main.R
@@ -0,0 +1,11 @@
+source("_targets.R")
+
+# If you have local cached objects (from running tar_make() previously)
+# uncomment the following line to delete the cache and calculate
+# everything from scratch.
+#
+# tar_destroy()
+
+# set the number of workers to a number not larger than the
+# number of threads your computer can comfortably run in parallel
+tar_make_future(workers = 32)
